6c932d38cabd3092fa052b8c410b0986e30dd634
DESKTOP-SHS2O79\test <blackwatch0522@gmail.com>
2024-03-03 02:29:32 +0900

    frontend init

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import React from "react";
+
+export type ButtonProps = { isDisabled: boolean };
+
+const Button: React.FC<ButtonProps> = ({
+    isDisabled = false, ...rest
+}) => {
+    return (
+        <div>1
+        </div>
+    );
+};
+
+export default Button;
